bc320b86c06cafbff7ab7c1682756255fcf05064
Enrique Folte <hhfolte@gmail.com>
2019-09-01 22:45:49 +0200

    add associations and seed data for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'categories_controller/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'categories#index'
+
+  resources :categories, only: [:index] do
+    resources :products, only: [:index]
+  end
 end

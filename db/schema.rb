81693d433f8990c3b53b83a1ace225d223ddc186
Enrique Folte <hhfolte@gmail.com>
2019-09-02 10:05:17 +0200

    add token to order with migration and delete create/destroy files in order_items view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_145835) do
+ActiveRecord::Schema.define(version: 2019_09_02_075716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_09_01_145835) do
     t.decimal "sub_total", precision: 15, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
   end
 
   create_table "product_categories", force: :cascade do |t|
